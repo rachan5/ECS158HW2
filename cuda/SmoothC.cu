dc69b1eecfd145644b70e48e3ca7f7a36f445d05
Bryan Stephen Ng <bryanng@pc43.cs.ucdavis.edu>
2015-03-01 01:27:44 -0800

    i think it works, still have to account for if the data wont fit into gpu

--- cuda/SmoothC.cu
+++ cuda/SmoothC.cu
@@ -1,12 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cuda.h>
 
 struct entry
 {
 	int origIndex;
-	float xValue;
-	float yValue;
+	float xValue, yValue;
 };//entry
+
+
+__device__ int binarySearch(entry * data, float val, int n, bool bound)
+{
+	//return index of xValue that is equal to or closest to value
+	//bound - round up or down, T = round down, F = round up
+	int left = 0;
+	int right = n;
+	int mid = (right-left)/2;
+
+	while(1)
+	{
+		if (data[mid].xValue == val)
+			return mid;
+
+		else if (data[mid].xValue > val)
+		{
+			if (mid == 0)
+				return mid;
+			else if ((data[mid-1].xValue < val) && (!bound))
+				return mid;	
+			else if ((data[mid-1].xValue < val) && (bound))
+				return mid-1;
+
+			right = mid;
+			mid = (right-left)/2;
+		}//else if
+
+		else if (data[mid].xValue < val)
+		{
+			if (mid == n-1)
+				return mid;
+			else if ((data[mid+1].xValue > val) && (!bound))
+				return mid+1;
+			else if ((data[mid+1].xValue > val) && (bound))
+				return mid;
+
+			left = mid;
+			mid = (right+left)/2;
+		}//else if
+	}//while
+}//binarySearch
+
+
+__global__ void kernel(entry * array, int n, float h)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x; 
+	int lowerBound = binarySearch(array, array[idx].xValue-h, n, 0); //T-round down, F-round up
+	int upperBound = binarySearch(array, array[idx].xValue+h, n, 1);
+
+	//check for duplicates
+	if (lowerBound != 0)
+	{
+		while (array[lowerBound-1].xValue == array[lowerBound].xValue)
+		{
+			lowerBound--;
+			if (lowerBound == 0)
+				break;
+		}//while
+	}//if
+
+	if (upperBound != n-1)
+	{
+		while (array[upperBound+1].xValue == array[upperBound].xValue)
+		{
+			upperBound++;
+			if (upperBound == n-1)
+				break;
+		}//while
+	}//if
+
+	float avg = 0;
+	//calculate y average
+	for (int i=lowerBound; i<upperBound+1; i++)
+		avg += array[i].yValue;
+	avg = avg/((float) (upperBound-lowerBound+1));
+
+	array[idx].yValue = avg;	
+}//kernel
+
 
 void merge(entry * a, int low, int high)
 {
@@ -53,6 +133,10 @@ void mergeSort(entry * a, int low, int high)
 void smoothc(float * x, float * y, float * m, int n, float h)
 {
 	entry * array = new entry[n];
+	entry * deviceArray;
+	float blockSize = 1024;
+
+	//creat array of structs	
 	for (int i=0; i<n; i++)
 	{
 		entry temp;
@@ -62,8 +146,27 @@ void smoothc(float * x, float * y, float * m, int n, float h)
 		array[i] = temp;
 	}//for
 	
+	//sort by x values
 	mergeSort(array, 0, n-1);
 
+	cudaMalloc(&deviceArray, sizeof(entry) * n);
+	cudaMemcpy(deviceArray, array, sizeof(entry) * n, cudaMemcpyHostToDevice);
+
+	dim3 dimBlock(blockSize);
+	dim3 dimGrid(ceil(n/blockSize));
+
+	//stores smoothed average in yValue
+	kernel <<< dimGrid, dimBlock >>>(deviceArray, n, h);
+
+	cudaMemcpy(array, deviceArray, sizeof(entry) * n, cudaMemcpyDeviceToHost);
+	
+	//rearrange array in original order
+	for (int i=0; i<n; i++)
+	{
+		m[array[i].origIndex] = array[i].yValue;
+	}//for
+
+	cudaFree(deviceArray);
 	delete [] array;
 }//smoothc
 
