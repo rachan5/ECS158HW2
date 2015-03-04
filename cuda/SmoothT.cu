93865881ccf41eb1b3b606b938ddf047de255024
Raymond S. Chan <raschan@ucdavis.edu>
2015-03-03 18:12:58 -0800

    kinda faster SmoothT.cu

--- cuda/SmoothT.cu
+++ cuda/SmoothT.cu
@@ -5,6 +5,7 @@
 #include <thrust/for_each.h>
 #include <thrust/iterator/zip_iterator.h>
 #include <thrust/copy.h>
+#include <thrust/reduce.h>
 #include <iostream>
 
 
@@ -17,32 +18,12 @@ struct entry
 struct smoother_functor
 {
   const float h;
-  //const float sum;
-  //const int nums_in_sum;
-  //smoother_functor(float _h, float _sum, int _nums_in_sum): h(_h), sum(_sum), nums_in_sum(_nums_in_sum){}
   const float x_i;
   smoother_functor(float _h, float _x_i): h(_h), x_i(_x_i){}
-  //template <typename Tuple>
   __host__ __device__
-  //float operator()(Tuple t)
   float operator()(const float&x, const float& y)const //FILL IN FROM TUTORIAL
   {
-    //if (abs(thrust::get<0>(t) - thrust::get<1>(t)) < h)
-    //{
-    //  //Y_j is stored here
-    //  sum += thrust::get<2>(t);
-    //  //nums_in_sum++;
-    //}
-    //else
-    //{
-    //  sum += 0;
-    //  nums_in_sum += 0;
-    //}
-
-    //answer will go here
-    //thrust::get<3>(t) = sum/nums_in_sum; 
-    //return 0;
-    return (abs(x-x_i) > h) ? 1 : 0;
+    return (fabs(x-x_i) < (float)h) ? 1.0 : 0.0;
   }
 }; //struct smoother_functor
 
@@ -51,36 +32,44 @@ void smootht(float *x, float *y, float *m, int n, float h)
   thrust::device_vector<float> dx(n,0.0);
   thrust::device_vector<float> dy(n,0.0);
   thrust::device_vector<float> dm(n,0.0);
-  thrust::device_vector<int> dtemp(n,0);
+  thrust::device_vector<float> dtemp(n,0.0);
   thrust::device_vector<float> dtemp2(n,0.0);
   //gotta initalize these arrays!
+  //std::cout << "Check initalization" << std::endl;
   for (int i = 0; i < n; i++)
   {
     *(dx.begin()+i) = x[i];
     *(dy.begin()+i) = y[i];
-    std::cout << *(dx.begin()+i) << " " << *(dy.begin()+i) << std::endl;
+    //std::cout << *(dx.begin()+i) << " " << *(dy.begin()+i) << std::endl;
   }
+  
   //iterate through x for x_i, pass into smoother_functor...
   for (int i = 0; i < dx.size(); i++)
   {
     float total = 0.0;
-    int count =0;
-    thrust::transform(dx.begin(),dx.end(), dtemp.begin(), dtemp.begin(),smoother_functor(h,*(dx.begin()+i)));
+    int count = 0;
+    thrust::transform(dx.begin(),dx.end(), dy.begin(), dtemp.begin(),smoother_functor(h,*(dx.begin()+i)));
+    //std::cout << "CHECK AVERAGING" << std::endl;
+   
+    //calculate averages
+    /*
     for (int j = 0; j < dtemp.size(); j++)
     {
-      if (*(dtemp.begin()+j))
+      if (*(dtemp.begin()+j) == 1)
       {
         total += *(dy.begin()+j);
         count++;
       }
     }//end for j
-    *(dm.begin()+i) = total/count;
-    std::cout << total << " " << count << " " << total/count << std::endl;
-  //  //thrust::fill(dtemp.begin(), dtemp.end(), dx.begin()+i);
-  //  //thrust::transform(dx.begin(),dx.end(), dtemp.begin(), dtemp2.end(),smoother_functor(h,));
+    *(dm.begin()+i) = (total/count);
+    */
+    thrust::transform(dy.begin(), dy.end(), dtemp.begin(), dtemp2.begin(), thrust::multiplies<float>());
+    total = thrust::reduce(dtemp2.begin(),dtemp2.end());
+    count = thrust::reduce(dtemp.begin(),dtemp.end());
+    *(dm.begin()+i) = (total/count);
+    
+    //std::cout << total << " " << count << " " << total/count << std::endl;
   }//end for i 
-  //thrust::for_each(thrust::make_zip_iterator(thrust::make_tuple(dx.begin(), dx.begin(), dy.begin(), dm.begin())),thrust::make_zip_iterator(thrust::make_tuple(dx.end(),   dx.end(),   dy.end(),   dm.end())),smoother_functor(h,0.0,0));
-
   thrust::copy(dm.begin(), dm.end(), std::ostream_iterator<float>(std::cout, "\n"));
 
 }//smootht
@@ -88,7 +77,7 @@ void smootht(float *x, float *y, float *m, int n, float h)
 
 int main()
 {
-	/*
+  	
   int a, n = 2000000;
 	float * x = new float[n];
 	float * y = new float[n];
@@ -102,14 +91,14 @@ int main()
 		x[i] = ((float)rand()/(float)(RAND_MAX)*2*a - a);
 		y[i] = ((float)rand()/(float)(RAND_MAX)*2*a - a);
 	}//generate random floats for x and y
-*/
-	
   
+	
+  /*
   float x[20] = {1, 1,2,2, 3,3, 4,4, 5,5, 6,6, 7,7, 8,8, 9,9, 10,10};
 	float y[20] = {11,11, 12,12, 13,13, 14,14, 15,15, 16,16, 17,17, 18,18, 19,19, 20,20};
 	float m[20];
 	int n = 20;
 	float h = 2;
-
+  */
 	smootht(x, y, m, n, h);
 }//main
